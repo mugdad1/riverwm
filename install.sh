ab237fa099d9307571f66af36b156bfe6bc61608
mugdad <mugdad02@tutamail.com>
2026-02-13 22:35:19 +0300

    gruvfisher

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
-sudo xbps-install -Syu river chafa wlroots alacritty Waybar wofi mako grim slurp fish-shell micro light  yazi viewnior ImageMagick polkit-gnome xorg-server-xwayland xdg-desktop-portal-wlr  pulsemixer elogind mesa-dri mesa-vulkan-intel seatd dunst xdg-user-dirs-gtk nerd-fonts pulseaudio wl-clipboard cliphist swaylock swayidle 
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-fisher install jomik/fish-gruvbox
+# Install packages
+sudo xbps-install -Syu river chafa wlroots alacritty Waybar wofi mako grim \
+slurp fish-shell micro light yazi viewnior ImageMagick polkit-gnome \
+xorg-server-xwayland xdg-desktop-portal-wlr pulsemixer elogind \
+mesa-dri mesa-vulkan-intel seatd dunst xdg-user-dirs-gtk nerd-fonts \
+pulseaudio wl-clipboard cliphist swaylock swayidle 
 
+# Using Fish shell to run Fisher commands
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+fish -c "fisher install jomik/fish-gruvbox"
 
+# Create necessary directories
 mkdir -p ~/.config/river/
 mkdir -p ~/Pictures/screenshots
-cp -r ~/riverwm/* ~/.config/river/
+mkdir -p ~/.config/micro/
+cp ~/riverwm/micro/*  ~/.config/micro/
+# Copy river configuration files
+cp -r ~/riverwm/*	 ~/.config/river/
 
-
+# Set executable permissions for init
 chmod +x ~/.config/river/init
-
