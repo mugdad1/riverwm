363b058f7ebb4230eba977a6cf21844286cfa488
mugdad <mugdad02@tutamail.com>
2026-02-13 22:03:51 +0300

    gruvfisher

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-sudo xbps-install -Syu river wlroots alacritty Waybar wofi mako grim slurp fish-shell micro light yad yazi viewnior ImageMagick polkit-gnome xorg-server-xwayland xdg-desktop-portal-wlr  pulsemixer elogind mesa-dri mesa-vulkan-intel seatd dunst xdg-user-dirs-gtk nerd-fonts pulseaudio wl-clipboard cliphist swaylock swayidle 
-
+sudo xbps-install -Syu river chafa wlroots alacritty Waybar wofi mako grim slurp fish-shell micro light  yazi viewnior ImageMagick polkit-gnome xorg-server-xwayland xdg-desktop-portal-wlr  pulsemixer elogind mesa-dri mesa-vulkan-intel seatd dunst xdg-user-dirs-gtk nerd-fonts pulseaudio wl-clipboard cliphist swaylock swayidle 
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install jomik/fish-gruvbox
 
 
 mkdir -p ~/.config/river/
