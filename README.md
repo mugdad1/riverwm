ed5ba5385ec1a6039aa3a5d3b9c95dc87c9352f3
mugdad <mugdad02@tutamail.com>
2026-02-13 21:03:20 +0300

    f

--- README.md
+++
@@ -1,8 +0,0 @@
-for void linux
-
-
-river wlroots alacritty swaybg Waybar wofi foot mako grim slurp light yad Thunar geany viewnior ImageMagick polkit-gnome xorg-server-xwayland xdg-desktop-portal-wlr rofi pulsemixer
-git base-devel elogind mesa-dri mesa-vulkan-intel seatd dunst xdg-user-dirs-gtk nerd-fonts pulseaudio wl-clipboard cliphist
-
-move all files to .config/river
-git clone https://squidnose/voidlinux-post-install-tui
--- install.sh
+++ install.sh
@@ -1,24 +1,11 @@
-sudo xbps-install -Syu --needed base-devel git
-sudo xbps-install -S --noconfirm river wlroots alacritty swaybg Waybar wofi foot mako grim slurp light yad Thunar geany viewnior ImageMagick polkit-gnome xorg-server-xwayland xdg-desktop-portal-wlr rofi pulsemixer
-git base-devel elogind mesa-dri mesa-vulkan-intel seatd dunst xdg-user-dirs-gtk nerd-fonts pulseaudio wl-clipboard cliphist thunar swaylock
+sudo xbps-install -Syu river wlroots alacritty  Waybar wofi foot mako grim slurp fish-shell light yad Thunar viewnior ImageMagick polkit-gnome xorg-server-xwayland xdg-desktop-portal-wlr  pulsemixer elogind mesa-dri mesa-vulkan-intel seatd dunst xdg-user-dirs-gtk nerd-fonts pulseaudio wl-clipboard cliphist swaylock swayidle 
 
 
 
-git clone https://github.com/mugdad1/River.git ~/Downloads/River
+mkdir -p ~/.config/river/
 
-mkdir -p ~/.config/{alacritty,dunst,moc,neofetch,pcmanfm,river,rofi,waybar}
-
-cp -r ~/Downloads/River/* ~/.config/river/
+cp -r ~/riverwm/* ~/.config/river/
 
 
 chmod +x ~/.config/river/init
-RED='\033[0;31m'
-
-# refind-install
-# git clone https://github.com/josephsurin/refind-theme-circle.git && sudo rm -r ./refind-theme-circle/{screenshots,.git}
-# sudo cp -r refind-theme-circle /boot/efi/EFI/refind/ && sudo echo "include refind-theme-circle/theme.conf" >> /boot/efi/EFI/refind/refind.conf
-
-sleep 5
-echo -e "${RED}Please Reboot Now."
-
 
