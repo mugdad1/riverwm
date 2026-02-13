4e65189436c0e46a590a2dced54c37c030c6fd37
mugdad <mugdad02@tutamail.com>
2026-02-13 21:24:23 +0300

    f

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo xbps-install -Syu river wlroots alacritty  Waybar wofi foot mako grim slurp fish-shell light yad Thunar viewnior ImageMagick polkit-gnome xorg-server-xwayland xdg-desktop-portal-wlr  pulsemixer elogind mesa-dri mesa-vulkan-intel seatd dunst xdg-user-dirs-gtk nerd-fonts pulseaudio wl-clipboard cliphist swaylock swayidle 
+sudo xbps-install -Syu river wlroots alacritty Waybar wofi mako grim slurp fish-shell micro light yad yazi viewnior ImageMagick polkit-gnome xorg-server-xwayland xdg-desktop-portal-wlr  pulsemixer elogind mesa-dri mesa-vulkan-intel seatd dunst xdg-user-dirs-gtk nerd-fonts pulseaudio wl-clipboard cliphist swaylock swayidle 
 
 
 
 mkdir -p ~/.config/river/
-
+mkdir -p ~/Pictures/screenshots
 cp -r ~/riverwm/* ~/.config/river/
 
 
